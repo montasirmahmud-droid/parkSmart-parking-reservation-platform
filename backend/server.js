8b9e1c53e1ccedb0c7c0283e77457bbfc1ddadc2
Tasmia Hossain <tasmiahossain575@gmail.com>
2026-04-09 23:28:36 +0600

    Connected MongoDB database to the main server.

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,15 @@ const app = express();              // 2. Start the "App" (the server)
 app.use(express.json()); // 3. Logic: This lets the server read JSON data from the user
 
 // --- NEW: DATABASE CONNECTION ---
-// This tells the server WHERE to save the data (Member 3 Finance logic needs this!)
+// This tells the server WHERE to save the data (Finance logic needs this!)
 mongoose.connect('mongodb://localhost:27017/parksmart')
     .then(() => console.log("Database Connected! ParkSmart is ready to save users."))
     .catch(err => console.log("Database Error: ", err));
 // --------------------------------
 
-// 4. Link your Member 3 Routes
+// 4. Link the Routes
 const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes); // This makes your signup live at /api/auth/signup
+app.use('/api/auth', authRoutes); // This makes signup live at /api/auth/signup
 
 // 5. Start the engine
 const PORT = 5000;
