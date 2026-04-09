2ccfcb86027f4f57a47301d7aafa0349db49c1dc
montasirmahmud-droid <montasir.mahmud@g.bracu.ac.bd>
2026-04-09 21:23:53 +0600

    Delete models/parking_record.py

--- models/parking_record.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-class ParkingRecord:
-    def __init__(self, vehicle_id: str, reserved_hours: float):
-        self.vehicle_id = vehicle_id
-        self.entry_time: datetime | None = None
-        self.exit_time: datetime | None = None
-        self.reserved_hours = reserved_hours
