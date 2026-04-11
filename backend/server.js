823f68b9b4d6c5c7152ed884eeaad74fb2bca2a4
Tasmia Hossain <tasmiahossain575@gmail.com>
2026-04-12 04:28:43 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,39 @@
-const express = require('express'); // 1. Load the web server tool
-const app = express();              // 2. Start the "App" (the server)
+const express = require('express'); // 1. Load the tool
+const mongoose = require('mongoose'); 
+const cors = require('cors');        // Load the security pass
 
-app.use(express.json()); // 3. Logic: This lets the server read JSON data from the user
+const app = express();              // 2. NOW start the "App" (the car is built)
 
-// 4. Link your Member 3 Routes
+// --- MIDDLEWARE (The settings) ---
+app.use(cors());                    // Now it's okay to use cors
+app.use(express.json());            // Lets the server read JSON data
+
+// --- DATABASE CONNECTION ---
+
+//const dbURI = 'mongodb://localhost:27017/parksmart'
+
+// from this part is for temp database
+const dbURI = "mongodb://SysAdmin_1:SysAdmin12345@ac-v0rralt-shard-00-00.zyigojp.mongodb.net:27017,ac-v0rralt-shard-00-01.zyigojp.mongodb.net:27017,ac-v0rralt-shard-00-02.zyigojp.mongodb.net:27017/?ssl=true&replicaSet=atlas-xlu1r0-shard-0&authSource=admin&appName=ParkSmartCluster"
+
+
+mongoose.connect(dbURI)
+    .then(() => console.log("Database Connected! ParkSmart is ready."))
+    .catch((err) => console.log("Database Error: ", err));
+
+// --- ROUTES ---
 const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes); // This makes your signup live at /api/auth/signup
+app.use('/api/auth', authRoutes); 
 
-// 5. Start the engine
+const financeRoutes = require('./routes/financeRoutes');
+app.use('/api/finance', financeRoutes);
+
+// --- START ENGINE ---
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}. ParkSmart is ready!`);
+    console.log(`Server is running on port ${PORT}. ParkSmart is alive!`);
 });
+
+
+//temp database didn't connect with these:
+//mongodb+srv://SysAdmin_1:<db_password>@parksmartcluster.zyigojp.mongodb.net/?appName=ParkSmartCluster
+//const dbURI = 'mongodb+srv://SysAdmin_1:SysAdmin12345@parksmartcluster.zyigojp.mongodb.net/?appName=ParkSmartCluster'
