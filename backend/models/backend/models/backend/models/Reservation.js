fa4040591a81b5d504ccc778e8277c33feac0a7d
montasirmahmud-droid <montasir.mahmud@g.bracu.ac.bd>
2026-04-19 20:39:55 +0600

    Create Reservation.js

---
+++ backend/models/backend/models/backend/models/Reservation.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const reservationSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  slot: { type: mongoose.Schema.Types.ObjectId, ref: "ParkingSlot" },
+  startTime: Date,
+  endTime: Date
+});
+
+export default mongoose.model("Reservation", reservationSchema);
