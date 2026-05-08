92acf7b1d94c58fb740f877cba6126f982cba5f9
Tasmia Hossain <tasmiahossain575@gmail.com>
2026-05-09 03:47:52 +0600

    Switch to CommonJS syntax for imports in recommendationRoutes

--- backend/routes/recommendationRoutes.js
+++ backend/routes/recommendationRoutes.js
@@ -1,5 +1,9 @@
-import express from "express";
-import { getRecommendedSlots } from "../controllers/recommendationController.js";
+const express = require("express");
+const { getRecommendedSlots } = require("../controllers/recommendationController");
+const { requireAuth } = require("../middleware/auth");
+
+//import express from "express";
+//import { getRecommendedSlots } from "../controllers/recommendationController.js";
 
 const router = express.Router();
 
