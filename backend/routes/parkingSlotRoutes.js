8317f1d4722d56fa69adbc4c6726b43cd70984db
Tasmia Hossain <tasmiahossain575@gmail.com>
2026-05-09 04:00:08 +0600

    Change import syntax from ES6 to CommonJS

--- backend/routes/parkingSlotRoutes.js
+++ backend/routes/parkingSlotRoutes.js
@@ -1,8 +1,12 @@
-import express from "express";
-import { getSlotById } from "../controllers/parkingSlotController.js";
+const express = require("express");
+const { getSlotById } = require("../controllers/parkingSlotController");
+
+//import express from "express";
+//import { getSlotById } from "../controllers/parkingSlotController.js";
 
 const router = express.Router();
 
 router.get("/:id", getSlotById);
 
 export default router;
+
