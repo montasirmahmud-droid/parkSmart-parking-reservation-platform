6bb26cdc52e53633476dd359ffdffc531812264b
Tasmia Hossain <tasmiahossain575@gmail.com>
2026-05-09 07:20:38 +0600

    Convert parkingSlotRoutes to ES6 module syntax

--- backend/routes/parkingSlotRoutes.js
+++ backend/routes/parkingSlotRoutes.js
@@ -1,12 +1,8 @@
-const express = require("express");
-const { getSlotById } = require("../controllers/parkingSlotController");
-
-//import express from "express";
-//import { getSlotById } from "../controllers/parkingSlotController.js";
+import express from "express";
+import { getSlotById } from "../controllers/parkingSlotController.js";
 
 const router = express.Router();
 
 router.get("/:id", getSlotById);
 
-module.exports = router;
-//export default router;
+export default router;
