60a451ea01de82b05843703f51048c30f257fb3f
Tasmia Hossain <tasmiahossain575@gmail.com>
2026-05-09 07:21:09 +0600

    Update recommendationRoutes.js

--- backend/routes/recommendationRoutes.js
+++ backend/routes/recommendationRoutes.js
@@ -1,13 +1,8 @@
-const express = require("express");
-const { getRecommendedSlots } = require("../controllers/recommendationController");
-const { requireAuth } = require("../middleware/auth");
-
-//import express from "express";
-//import { getRecommendedSlots } from "../controllers/recommendationController.js";
+import express from "express";
+import { getRecommendedSlots } from "../controllers/recommendationController.js";
 
 const router = express.Router();
 
 router.get("/", getRecommendedSlots);
 
-module.exports = router;
-//export default router;
+export default router;
